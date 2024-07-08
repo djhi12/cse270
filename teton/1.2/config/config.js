0463c0219c77c1cdfbaf87367269f23a5f3233ab
djhi12 <ido20002@byui.edu>
2024-07-08 22:08:42 +0800

    updated

--- teton/1.2/config/config.js
+++ teton/1.2/config/config.js
@@ -1,14 +1,14 @@
 const LAT = "43.8866";
 const LON = "-111.6777";
-const APIKEY = "<PROVIDE_KEY_HERE>";
+const APIKEY = "7f21af65ffe10b758318e3fbb459edda";
 
 const WEATHER_URL_STUB = "./data/weather.json";
 const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
 
-const apiURL = WEATHER_URL_STUB;
+const apiURL = WEATHER_URL_PROD;
 
 const DIRECTORY_DATA_URL_PROD = "<PRODUCTION_URL_HERE>";
 const DIRECTORY_DATA_URL_STUB = "./data/business.json";
 const DIRECTORY_DATA_URL_TEST = "http://127.0.0.1:8000/data/all";
 
-const businessDataUrl = DIRECTORY_DATA_URL_STUB;
+const businessDataUrl = DIRECTORY_DATA_URL_TEST;
